b137845abc77c40b0964550ddf20d1dcc2ebafc8
Madiyara <samarbekova92@gmail.com>
2019-05-07 13:13:27 -0500

    added new file

---
+++ datapracticefun/src/main/java/com/triology/DataTypePractice.java
@@ -0,0 +1,18 @@
+package com.triology;
+
+public class DataTypePractice {
+
+    public static void main(String[] args) {
+      String addressandcity = "339 E Chicago, Chicago, IL 60611";
+
+
+
+
+
+
+    }
+
+
+
+
+}
